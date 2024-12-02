a591a05251a771ceb919884bc72d70f654fd339c
Fruithy <Magnusrobbestad@hotmail.com>
2024-12-02 02:29:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function sendInterest(domain) {
-    const emailBody = `Hei, jeg er interessert i domenenavnet ${domain}.`;
-    const mailtoLink = `mailto:Kontakt@Navnhuset.no?subject=Interesse for domenenavn&body=${encodeURIComponent(emailBody)}`;
-    window.location.href = mailtoLink;
-}
