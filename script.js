fdca8ed48847b297e899ab91bd4ac11d8e9b95b8
Fruithy <Magnusrobbestad@hotmail.com>
2024-12-06 15:52:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@
 // script.js
 function kontaktOss(domenenavn) {
     alert(`Takk for din interesse for ${domenenavn}!
-Send oss en e-post på kontakt@Navnhuset.no for mer informasjon.`);
+Send oss en e-post på Magnusrobbestad@gmail.com for mer informasjon.`);
 }
